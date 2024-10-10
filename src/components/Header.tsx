42cc02fb2a4514fa8b527247edfe7a721b05dcc4
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-10-09 17:03:28 -0700

    update: updated assets on sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import React from 'react';
 import wallet from '../assets/icons/wallet.svg';
 
-
 const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center p-4 text-white">
@@ -14,7 +13,7 @@ const Header: React.FC = () => {
           className="w-[407px] p-2 rounded-lg bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
         />
       </div>
-      <button className="flex items-center bg-purple-600 px-4 py-2 rounded">
+      <button className="flex items-center bg-primary-400 px-4 py-2 rounded">
         <Image src={wallet} alt="Wallet" width={18} height={18} className="mr-2" />
         Connect Wallet
       </button>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
-import { FaBook, FaUsers, FaStore, FaCoins, FaUser } from 'react-icons/fa';
 import Image from 'next/image';
+
+import book from '../assets/icons/book.svg';
+import users from '../assets/icons/users.svg';
+import photo from '../assets/icons/photos.svg';
+import coin from '../assets/icons/coin.svg';
+import user from '../assets/icons/user.svg';
+
+const sidebarItems = [
+  { icon: book, label: 'Classroom' },
+  { icon: users, label: 'Community' },
+  { icon: photo, label: 'Marketplace' },
+  { icon: coin, label: 'Reward' },
+  { icon: user, label: 'Profile' },
+];
 
 const Sidebar: React.FC = () => {
   return (
-    <nav className="bg-gray-900 text-white w-64 w-64 h-96 rounded-2xl shadow-inner backdrop-blur-3xl m-6">
+    <nav className="bg-gray-900 text-white w-64 h-96 rounded-2xl shadow-inner backdrop-blur-3xl m-6">
       <header className="pl-4 pr-20 py-3 border-b border-[#7b50ea]/25 flex items-center">
         <div className="flex items-center gap-3.5">
           <Image src={logo} alt="DeChange Logo" width={44} height={44} />
-          <h1 className="text-center text-[#7b50ea] text-2xl font-bold font-['Figtree']">DeChange</h1>
+          <h1 className="text-center text-primary-400 text-2xl font-bold">DeChange</h1>
         </div>
       </header>
 
-      <ul className="space-y-2 p-4">
-        <li className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-          <FaBook className="mr-2" />
-          <span>Classroom</span>
-        </li>
-        <li className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-          <FaUsers className="mr-2" />
-          <span>Community</span>
-        </li>
-        <li className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-          <FaStore className="mr-2" />
-          <span>Marketplace</span>
-        </li>
-        <li className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-          <FaCoins className="mr-2" />
-          <span>Reward</span>
-        </li>
-        <li className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-          <FaUser className="mr-2" />
-          <span>Profile</span>
-        </li>
+      <ul className="space-y-2 p-4 border-t border-[#7b50ea]/25 pt-3 mt-10">
+        {sidebarItems.map((item, index) => (
+          <li key={index} className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
+            <Image src={item.icon} alt={item.label} width={28} height={28} className="mr-2" />
+            <span className="font-medium text-gray-600 text-lg">{item.label}</span>
+          </li>
+        ))}
       </ul>
     </nav>
   );
