44fe4579c21ab8ac600f365ec188788a24ea0aa6
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-10-11 05:52:38 -0700

    update: development

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ import Image from 'next/image';
 
 import shadow from '../assets/images/bg-shadow.svg'; 
 
-
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen relative">
       <div className="flex flex-1">
-        <Sidebar />
-        <div className="flex-1 p-4 z-10 mb-[82px]">
+        <div className="fixed h-full mr-4">
+          <Sidebar />
+        </div>
+        <div className="flex-1 p-4 z-10 mb-[82px] ml-72">
           <Header />
           <Tags />
           <div className="grid grid-cols-4 gap-2 mt-8">
@@ -23,7 +24,11 @@ const Home: React.FC = () => {
             <CourseCard title="Course title" level="Beginner" />
             <CourseCard title="Course title" level="Advanced" />
           </div>
-          <h2 className="text-xl font-bold mt-8">Top Picks • Courses taken by new users</h2>
+          <div className="h-7 justify-start items-center mt-9 gap-2 inline-flex">
+            <div className="text-white text-2xl font-semibold">Top Picks</div>
+            <div className="w-1 h-1 bg-[#d9d9d9] rounded-full" />
+            <div className="text-gray-600 text-base font-normal">Courses taken by new users</div>
+          </div>
           <div className="grid grid-cols-4 gap-2 mt-4">
             <CourseCard title="Course title" level="Beginner" />
             <CourseCard title="Introduction to DeFi" level="Beginner" />
@@ -32,7 +37,7 @@ const Home: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="absolute inset-x-0 top-0 h-[100vh] translate-y-[340px] z-0 overflow-hidden">
+      <div className="absolute inset-x-0 top-0 h-[100vh] translate-y-[350px] z-0 overflow-hidden">
         <Image src={shadow} alt="bg-shadow" layout="fill" objectFit="cover" />
       </div>
       <Footer />
