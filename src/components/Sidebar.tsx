10ab981574b4202ec8500327a798b6acb8e654ff
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-10-11 06:38:38 -0700

    update: development

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/images/logo.svg';
 import Image from 'next/image';
 
@@ -17,6 +17,8 @@ const sidebarItems = [
 ];
 
 const Sidebar: React.FC = () => {
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <nav className="bg-gray-900 border-[#7B51EA2E] text-white w-64 h-96 rounded-2xl shadow-inner backdrop-blur-3xl m-6">
       <header className="pl-4 pr-20 py-3 border-b border-[#7b50ea]/25 flex items-center">
@@ -28,9 +30,23 @@ const Sidebar: React.FC = () => {
 
       <ul className="space-y-2 p-4 border-t border-[#7b50ea]/25 pt-3 mt-10">
         {sidebarItems.map((item, index) => (
-          <li key={index} className="flex items-center p-2 rounded-lg hover:bg-gradient-to-r from-purple-600 to-purple-400">
-            <Image src={item.icon} alt={item.label} width={28} height={28} className="mr-2" />
-            <span className="font-medium text-gray-600 text-lg">{item.label}</span>
+          <li
+            key={index}
+            className={`flex items-center p-2 rounded-lg cursor-pointer transition-all duration-200 ${
+              activeItem === index
+                ? 'bg-gradient-to-r from-gray-900 to-[#452D84] text-primary-400 shadow-inner border-[#7B51EA2E] '
+                : 'hover:bg-gradient-to-r from-gray-900 to-[#452D84] text-gray-600'
+            }`}
+            onClick={() => setActiveItem(index)}
+          >
+            <Image
+              src={item.icon}
+              alt={item.label}
+              width={28}
+              height={28}
+              className={`mr-2 ${activeItem === index ? 'text-purple-400' : 'text-gray-600'}`}
+            />
+            <span className="font-medium text-lg">{item.label}</span>
           </li>
         ))}
       </ul>
