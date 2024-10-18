6a110d603e70a6e0e9515a0eac9c32775dc693f0
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-10-18 05:01:34 -0700

    update: development

--- src/pages/course/sections/IntroductionToDefi/Practice.tsx
+++ src/pages/course/sections/IntroductionToDefi/Practice.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import React from 'react'
+
+import arrowRight from '../../../../assets/icons/arrow-side.svg'
 
 const Practice: React.FC = () => {
   return (
@@ -15,20 +18,11 @@ const Practice: React.FC = () => {
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-4 p-2.5 rounded-2xl bg-transparent">
           <div className="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0">
             <div className="flex justify-start items-center flex-grow relative gap-[7px]">
-              <svg
-                width={18}
-                height={19}
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative"
-                preserveAspectRatio="xMidYMid meet"
-              >
-                <path
-                  d="M6.75 13.6896L6.75 5.31061C6.75003 5.1623 6.79403 5.01733 6.87645 4.89402C6.95886 4.77071 7.07598 4.67461 7.21301 4.61786C7.35003 4.5611 7.50081 4.54625 7.64627 4.57518C7.79174 4.6041 7.92536 4.67551 8.03025 4.78036L12.2197 8.96986C12.3603 9.11051 12.4393 9.30124 12.4393 9.50011C12.4393 9.69899 12.3603 9.88972 12.2197 10.0304L8.03025 14.2199C7.92536 14.3247 7.79174 14.3961 7.64627 14.4251C7.50081 14.454 7.35003 14.4391 7.21301 14.3824C7.07598 14.3256 6.95886 14.2295 6.87645 14.1062C6.79404 13.9829 6.75003 13.8379 6.75 13.6896Z"
-                  fill="#DBDADB"
-                />
-              </svg>
+              <Image
+                src={arrowRight}
+                alt="Arrow"
+                className="flex-grow-0 flex-shrink-0"
+              />
               <p className="flex-grow w-full text-xl font-medium text-left text-white">
                 Claim faucet at Alchemy
               </p>
@@ -45,20 +39,11 @@ const Practice: React.FC = () => {
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-4 p-2.5 rounded-2xl bg-transparent">
           <div className="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0">
             <div className="flex justify-start items-center flex-grow relative gap-[7px]">
-              <svg
-                width={18}
-                height={19}
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative"
-                preserveAspectRatio="xMidYMid meet"
-              >
-                <path
-                  d="M6.75 13.6896L6.75 5.31061C6.75003 5.1623 6.79403 5.01733 6.87645 4.89402C6.95886 4.77071 7.07598 4.67461 7.21301 4.61786C7.35003 4.5611 7.50081 4.54625 7.64627 4.57518C7.79174 4.6041 7.92536 4.67551 8.03025 4.78036L12.2197 8.96986C12.3603 9.11051 12.4393 9.30124 12.4393 9.50011C12.4393 9.69899 12.3603 9.88972 12.2197 10.0304L8.03025 14.2199C7.92536 14.3247 7.79174 14.3961 7.64627 14.4251C7.50081 14.454 7.35003 14.4391 7.21301 14.3824C7.07598 14.3256 6.95886 14.2295 6.87645 14.1062C6.79404 13.9829 6.75003 13.8379 6.75 13.6896Z"
-                  fill="#DBDADB"
-                />
-              </svg>
+              <Image
+                src={arrowRight}
+                alt="Arrow"
+                className="flex-grow-0 flex-shrink-0"
+              />
               <p className="flex-grow w-full text-xl font-medium text-left text-white">
                 Swap tokens at Uniswap
               </p>
@@ -75,20 +60,11 @@ const Practice: React.FC = () => {
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-4 p-2.5 rounded-2xl bg-transparent">
           <div className="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0">
             <div className="flex justify-start items-center flex-grow relative gap-[7px]">
-              <svg
-                width={18}
-                height={19}
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative"
-                preserveAspectRatio="xMidYMid meet"
-              >
-                <path
-                  d="M6.75 13.6896L6.75 5.31061C6.75003 5.1623 6.79403 5.01733 6.87645 4.89402C6.95886 4.77071 7.07598 4.67461 7.21301 4.61786C7.35003 4.5611 7.50081 4.54625 7.64627 4.57518C7.79174 4.6041 7.92536 4.67551 8.03025 4.78036L12.2197 8.96986C12.3603 9.11051 12.4393 9.30124 12.4393 9.50011C12.4393 9.69899 12.3603 9.88972 12.2197 10.0304L8.03025 14.2199C7.92536 14.3247 7.79174 14.3961 7.64627 14.4251C7.50081 14.454 7.35003 14.4391 7.21301 14.3824C7.07598 14.3256 6.95886 14.2295 6.87645 14.1062C6.79404 13.9829 6.75003 13.8379 6.75 13.6896Z"
-                  fill="#DBDADB"
-                />
-              </svg>
+              <Image
+                src={arrowRight}
+                alt="Arrow"
+                className="flex-grow-0 flex-shrink-0"
+              />
               <p className="flex-grow w-full text-xl font-medium text-left text-white">
                 Bridge into Base
               </p>
@@ -105,20 +81,11 @@ const Practice: React.FC = () => {
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-4 p-2.5 rounded-2xl bg-transparent">
           <div className="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0">
             <div className="flex justify-start items-center flex-grow relative gap-[7px]">
-              <svg
-                width={18}
-                height={19}
-                viewBox="0 0 18 19"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="flex-grow-0 flex-shrink-0 w-[18px] h-[18px] relative"
-                preserveAspectRatio="xMidYMid meet"
-              >
-                <path
-                  d="M6.75 13.6896L6.75 5.31061C6.75003 5.1623 6.79403 5.01733 6.87645 4.89402C6.95886 4.77071 7.07598 4.67461 7.21301 4.61786C7.35003 4.5611 7.50081 4.54625 7.64627 4.57518C7.79174 4.6041 7.92536 4.67551 8.03025 4.78036L12.2197 8.96986C12.3603 9.11051 12.4393 9.30124 12.4393 9.50011C12.4393 9.69899 12.3603 9.88972 12.2197 10.0304L8.03025 14.2199C7.92536 14.3247 7.79174 14.3961 7.64627 14.4251C7.50081 14.454 7.35003 14.4391 7.21301 14.3824C7.07598 14.3256 6.95886 14.2295 6.87645 14.1062C6.79404 13.9829 6.75003 13.8379 6.75 13.6896Z"
-                  fill="#DBDADB"
-                />
-              </svg>
+              <Image
+                src={arrowRight}
+                alt="Arrow"
+                className="flex-grow-0 flex-shrink-0"
+              />
               <p className="flex-grow w-full text-xl font-medium text-left text-white">
                 Bridge into Base
               </p>
