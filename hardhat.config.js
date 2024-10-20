be5c490034ee2a2c73b9b7b759d92784e82484fa
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-10-20 02:11:06 -0700

    update: development

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
-require("@nomicfoundation/hardhat-ethers");
-require('@nomicfoundation/hardhat-toolbox');
-require("dotenv").config();  // Load environment variables
+require('@nomicfoundation/hardhat-ethers')
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config() // Load environment variables
 
-const { ALCHEMY_RPC_URL, PRIVATE_KEY } = process.env;
+const { ALCHEMY_RPC_URL, PRIVATE_KEY } = process.env
 
 module.exports = {
-  solidity: "0.8.1",
+  solidity: '0.8.1',
   networks: {
     baseSepolia: {
-      url: ALCHEMY_RPC_URL,  // Use Base Sepolia RPC URL from .env
-      chainId: 534351,       // Base Sepolia chain ID
-      accounts: [PRIVATE_KEY],  // Private key from .env
+      url: [process.env.ALCHEMY_RPC_URL], // Use Base Sepolia RPC URL from .env
+      chainId: 534351, // Base Sepolia chain ID
+      accounts: [process.env.PRIVATE_KEY], // Private key from .env
     },
   },
-};
+}
